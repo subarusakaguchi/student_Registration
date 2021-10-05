97bc2a6fe3456ebda2c1ebc56e927262617ddfc7
Subaru Sakaguchi <subarusakaguchi@yahoo.com.br>
2021-10-05 10:24:56 -0300

    Adicionado comandos de post, get e delete
    Falta a lógica de verificação de id

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express')
 const path = require('path')
-const student = require('./students/students')
+const students = require('./students/students')
 const app = express()
 const PORT = 8000
 
-
+app.use(express.urlencoded())
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/all', (req, res) => {
+    res.json(JSON.stringify(students.getAll()))
+})
+
+app.post('/new', (req, res) => {
+    let name = req.body.name
+    let surname = req.body.surname
+    let birth = req.body.birth
+    let address = req.body.address
+
+    students.newStudent(name, surname, birth, address)
+    res.send('Estudante adicionado')
+})
+
+app.delete('/del', (req, res) => {
+    let id = req.body.id
+
+    students.deleteStudent(id)
+    res.send('Estudante apagado')
+})
 
 app.listen(PORT, () => {
     console.log('Servidor rodando na porta: ' + PORT)
--- students/students.js
+++ students/students.js
@@ -1,35 +1,45 @@
-const students = {
+module.exports = {
     studentList: [{
-        id: 'aldasjad',
+        id: 'gwsgfsd',
         name: 'Pedro Paulo',
         surname: 'Pedreira Polinário',
         birth: '10/04/2002',
         address: 'Rua das flores nº 915'
     }],
 
-    all: function() {
+    getAll: function() {
         return this.studentList
     },
 
     newStudent: function(name, surname, birth, address) {
-        this.studentList.push({id: this.generateId(), name, surname, birth, address})
+        let id = this.generateId()
+        console.log(id)
+        let verifiedId = ''
+        verifiedId = this.verifyId(id)
+        console.log(verifiedId)
+
+        this.studentList.push({id: verifiedId, name, surname, birth, address})
+    },
+
+    deleteStudent: function(id) {
+        this.studentList = this.studentList.filter((student) => {
+            return student.id != id
+        })
     },
 
     generateId: function() {
-        let id = ''
-        while (this.verifyId(id) || id == '') {
-            id = Math.random().toString(36).substr(2, 9)
-        }
-        return id
+        console.log('teste1')
+        return id = Math.random().toString(36).substr(2, 9)
     },
 
     verifyId: function(id) {
         this.studentList.forEach((student) => {
-            if (id == student.id) {
-                return true
+            if (student.id == id) {
+                console.log('teste2')
+                return this.generateId()
             }
         })
-
-        return false
+        console.log('teste3')
+        return id
     }
 }
